d12061faa772b1c81ff31966d3b46f1375f72b4b
Darius Garcia <dariusgarcia888@gmail.com>
2022-11-07 18:56:47 -0800

    feat: add spline.design 3d model to about section

---
+++ script.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime'
+
+const canvas = document.getElementById('canvas3d')
+const app = new Application(canvas)
+app.load('https://prod.spline.design/6IcN6I8oOqHh40BT/scene.splinecode')
